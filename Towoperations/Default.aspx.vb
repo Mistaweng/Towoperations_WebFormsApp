a84f8e300d1be8b5ebfba4e0a2c7c5a18af6608d
africatcf <dev@africatcf.org>
2023-09-24 00:22:52 +0100

    Added the logoc that fetches data from the database and stores in the gridview

--- Towoperations/Default.aspx.vb
+++ Towoperations/Default.aspx.vb
@@ -2,6 +2,21 @@
     Inherits Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        'Load up the initial list of items
+        Dim strConn As String = ConfigurationManager.ConnectionStrings("tow_conn").ConnectionString
+        Dim objConn As New SqlClient.SqlConnection(strConn)
+        Dim dst As New DataSet()
+        Dim strSQL As String = "Select * From tow_jobs order by CalledInBy;"
+        Dim dpt As New SqlClient.SqlDataAdapter(strSQL, objConn)
+        dpt.Fill(dst, "ttmpTowJobs")
+        Dim tblData As DataTable = dst.Tables("ttmpTowJobs")
+        gvmMain.DataSource = tblData
+        gvmMain.DataBind()
+        objConn.Close()
+        tblData.Dispose()
+        dpt.Dispose()
+        objConn.Dispose()
+
 
     End Sub
 End Class
